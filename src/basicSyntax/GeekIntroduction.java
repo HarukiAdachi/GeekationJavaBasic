cd21a20e64a91351b25059248fd5c0af6041cf12
user <user@Nu>
2024-12-10 13:45:29 +0900

    add-geek-introduction-classの作成と実装。

---
+++ src/basicSyntax/GeekIntroduction.java
@@ -0,0 +1,15 @@
+package basicSyntax;
+
+public class GeekIntroduction {
+
+	public static void greeting(int age, double height) {
+
+		System.out.println("こんにちは！私はGeek君でごわす。");
+		
+		System.out.println("年は" + age + "でごわす。");
+		
+		System.out.println("身長は" + height + "でごわす。\n");
+
+	}
+
+}
---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,22 @@
+package basicSyntax;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int age = 25;
+		double height = 334.5;
+
+		GeekIntroduction.greeting(age, height);
+		
+		
+		
+		int num1=3;
+		int num2=299;
+		
+		SpecialSkill.spacial(num1,num2);
+		
+	}
+
+}
